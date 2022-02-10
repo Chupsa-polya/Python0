33fb16ed33e372873d94216ba0ee9ecadb5508c2
Chupsa-Polya <Chupsa-polkak@mail.ru>
2022-02-10 12:11:40 +0300

    ex00

---
+++ ex00/input.py
@@ -0,0 +1,19 @@
+#2 
+#6.8 
+#, 
+# Output 2, 6.8
+# 
+#1000
+#3.2
+#; 
+# output 1000; 3.2
+# 
+#
+# 
+#,
+#  output , 
+# 
+a = int(input())
+b = float(input())
+c = input()  
+print(a, c, b, sep = "")
